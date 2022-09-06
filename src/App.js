ae9d4ce97df8a06ab0ee7c625b32bffedbcf20d3
Şevval Buğdaycı <sevval.bugdayci@btsgrp.com>
2022-09-06 14:39:11 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,150 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useEffect, useState} from "react";
+import {socket, connectWebSocket} from './websocket'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+ChartJS.register(ArcElement, Tooltip, Legend);
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data,setData]=useState( {
+        labels: ['A Partisi', 'B Partisi', 'C Partisi', 'D Partisi', 'E Partisi', 'F Partisi'],
+        datasets: [
+            {
+                label: '# of Votes',
+                data: [50, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                ],
+                borderWidth: 1,
+            },
+        ],
+    })
+    const [text,setText]=useState("")
+    const [photo,setPhoto]=useState("")
+    const [socketInput, setSocketInput] = useState("")
+    const [socketText,setSocketText]=useState("")
+    useEffect(() => {
+
+        connectWebSocket()
+        socket.on('typeText',(data)=>{
+            setSocketText(data)
+        })
+        socket.on('broadcastPhoto',(data)=>{
+            if(data.id !== window.socketID){
+                setPhoto(data.photo)
+            }
+        })
+        socket.on('dataResult',(dataSet)=>{
+            let _data=data.datasets[0].data
+            for (let i=0;i<_data.length;i++){
+                _data[i]+=dataSet[i]
+            }
+            setData({...data,datasets:[ {
+                    label: '# of Votes',
+                    data: _data,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                    ],
+                    borderWidth: 1,
+                },] })
+            console.log('data',data)
+        })
+    }, [])
+    return (
+        <>
+            <button onClick={() => {
+                socket.emit('client', 'Merhaba ben client sana bilgi gönderiyorum')
+            }}>Servere Mesaj Gönder
+            </button>
+
+            <input value={socketInput} onChange={(e) => {
+                setSocketInput(e.target.value)
+            }}/>
+
+            <button onClick={() => {
+                socket.emit('sendCustomMessage', {socketId: socketInput, message: 'Merhaba '})
+            }}>Spesifik Bir Kullanıcıya Mesaj Gönder
+            </button>
+
+
+            <button onClick={() => {
+                socket.emit('publicMessage', {id: window.socketID, message: Date.now()})
+            }}>Yayınla
+            </button>
+
+            <button onClick={() => {
+                socket.emit('join room', 'roomA')
+            }}> A Odasına Katıl
+            </button>
+
+            <button onClick={() => {
+                socket.emit('message room', {room:'roomA',msg:'Merhabaaaaa'})
+            }}> A Odasına Mesaj Gönder
+            </button>
+
+            <button onClick={() => {
+                fetch('http://localhost:5000?text=Merhaba&id='+window.socketID).then((res)=>res.json()).then((data)=>{
+                    console.log('data',data)
+                })
+            }}> Kayıt Olustur
+            </button>
+
+            <button onClick={() => {
+                fetch('http://localhost:5000/sendPhoto?photo=https://images.unsplash.com/photo-1645583918683-39fd75293e80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1183&q=80=&id='+window.socketID).then((res)=>res.json()).then((data)=>{
+                    console.log('data',data)
+                })
+            }}> Fotoğraf Gönder
+            </button>
+            <div style={{display:"flex", alignItems:"center",justifyContent:"center"}}>
+                <div style={{width:"500px",height:"500px"}}>
+                    <Doughnut data={data} />;
+                </div>
+            </div>
+
+            <img width={500} height={500} src={photo}/>
+
+
+            <input value={text} onChange={(e) => {
+                setText(e.target.value)
+                socket.emit('text', e.target.value)
+
+            }}/>
+
+
+            {socketText}
+
+
+
+
+
+        </>
+
+    );
 }
 
 export default App;
---
+++ src/websocket.js
@@ -0,0 +1,79 @@
+import socketClient from 'socket.io-client'
+
+let socket;
+const SERVER='http://localhost:5000'
+const logToConsole=(data)=>{
+    console.log('data',new Date(data))
+}
+function notify(text){
+        alert(text)
+    if (Notification.permission !== "granted") {
+        Notification.requestPermission();
+    }
+    else{
+        var notification = new Notification('hello', {
+            body: 'Hey yeni bir kayıt olusturuldu içeriğe bir göz at' + text,
+        });
+        notification.onclick = function () {
+            window.open("http://google.com");
+        };
+    }
+}
+export const connectWebSocket=()=>{
+    socket=socketClient(SERVER,{
+        timeout:500000,
+        reconnectionDelay:3000,
+        path:'/my-custom-path/', extraHeaders: {
+            "my-custom-header": "1234"
+        }})
+
+    socket.on('onConnect',(data)=>{
+        console.log(data)
+        window.socketID=data
+    })
+    socket.on('hey',(data)=>{
+        console.log('data',data)
+    })
+
+    socket.on('publicMessage',(data)=>{
+        if(data.id !== window.socketID){
+            console.log('publicMessage',data)
+        }
+    })
+    socket.on('publicMessage2',(data)=>{
+        if(data.id !== window.socketID){
+            console.log(data)
+        }
+    })
+
+    socket.on('newRecord',(data)=>{
+        if(data.id !== window.socketID){
+            notify(data.text)
+        }
+    })
+
+
+    socket.on('joinedRoom',(data)=>{
+        console.log('data',data)
+    })
+
+
+    socket.on('roomMessage',(data)=>{
+        console.log('dataRoom',data)
+    })
+
+
+
+
+
+    socket.on('message',(data)=>{
+        console.log('Spesifik Mesaj',data)
+    })
+    socket.once('time',logToConsole)
+   // socket.off('time',logToConsole)
+
+   // socket.removeListener('time')
+
+ //   socket.removeAllListeners('time')
+}
+export {socket}
